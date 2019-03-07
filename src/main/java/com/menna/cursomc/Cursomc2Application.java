229896115df52bdb31ae22fb29739d7b367568ab
Ricardo M Menna <ricardo.menna@gmail.com>
2019-03-07 13:15:48 -0300

    Criando operacao de instanciacao

--- src/main/java/com/menna/cursomc/Cursomc2Application.java
+++ src/main/java/com/menna/cursomc/Cursomc2Application.java
@@ -1,13 +1,30 @@
 package com.menna.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc2Application {
+import com.menna.cursomc.domain.Categoria;
+import com.menna.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomc2Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
--- src/main/java/com/menna/cursomc/services/CategoriaService.java
+++ src/main/java/com/menna/cursomc/services/CategoriaService.java
@@ -2,7 +2,6 @@ package com.menna.cursomc.services;
 
 import java.util.Optional;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
