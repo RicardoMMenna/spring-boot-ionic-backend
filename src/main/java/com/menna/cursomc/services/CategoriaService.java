d8baf7f7b86f8814ce1667e66da5d2cd21bd6a6c
Ricardo M Menna <ricardo.menna@gmail.com>
2019-03-07 13:09:53 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/menna/cursomc/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.menna.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.menna.cursomc.domain.Categoria;
+import com.menna.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.get();
+	}
+
+	
+	
+}
